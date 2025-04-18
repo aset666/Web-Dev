221347e94e09271b16fcea4d79d366d2d589efaa
Assadbek Ravshanbekov <asadbekravshanbek70@gmail.com>
2025-04-18 12:05:31 +0300

    Add files via upload

---
+++ lab10/hh_back/task_m_ss/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TaskMSConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'task_m_s'
---
+++ lab10/hh_back/task_m_ss/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import index, hello
+
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('hello', hello, name='hello'),    
+]
+
+
